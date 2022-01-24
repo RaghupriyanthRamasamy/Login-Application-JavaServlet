c609747571b9986b5c6a4f344a697e46c5df628f
RaghupriyanthRamasamy <raghupriyanth@gmail.com>
2022-01-24 16:12:44 +0530

    Trash class to keep email in memory

---
+++ src/main/java/com/zc/accessvariables/AccessVariables.java
@@ -0,0 +1,15 @@
+package com.zc.accessvariables;
+
+public class AccessVariables {
+	
+	static String email;
+
+	public static String getEmail() {
+		return email;
+	}
+
+	public static void setEmail(String email) {
+		AccessVariables.email = email;
+	}
+	
+}
