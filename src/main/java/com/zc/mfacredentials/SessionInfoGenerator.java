e2336381216eb2304ac09582cb81feb5c1f49b68
RaghupriyanthRamasamy <raghupriyanth@gmail.com>
2022-01-25 20:22:41 +0530

    Code to generate otp session info for a user

---
+++ src/main/java/com/zc/mfacredentials/SessionInfoGenerator.java
@@ -0,0 +1,20 @@
+package com.zc.mfacredentials;
+
+import java.security.SecureRandom;
+
+public class SessionInfoGenerator {
+	public String generateSessionInfo() {
+		int n = 20;
+		String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "0123456789" + "abcdefghijklmnopqrstuvxyz";
+		StringBuilder sb = new StringBuilder(n);
+		
+		SecureRandom random = new SecureRandom();
+		
+		for (int i = 0; i < n; i++) {
+	        int rndCharAt = random.nextInt(AlphaNumericString.length());
+	        char rndChar = AlphaNumericString.charAt(rndCharAt);
+	        sb.append(rndChar);
+	    }
+		return sb.toString();
+	}
+}
