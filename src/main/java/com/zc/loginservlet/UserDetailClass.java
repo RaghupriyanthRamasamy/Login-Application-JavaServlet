039572f032ac0e7bb4a7f1d0a997a3a80c90c9da
RaghupriyanthRamasamy <raghupriyanth@gmail.com>
2022-01-25 20:18:31 +0530

    Update code to get userid through email, get username, set otp, validateOTP

--- src/main/java/com/zc/loginservlet/UserDetailClass.java
+++ src/main/java/com/zc/loginservlet/UserDetailClass.java
@@ -18,47 +18,47 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 
 public class UserDetailClass {
-	
+
 	public static final String[] HEADERS_TO_TRY = { "X-Forwarded-For", "Proxy-Client-IP", "WL-Proxy-Client-IP",
 			"HTTP_X_FORWARDED_FOR", "HTTP_X_FORWARDED", "HTTP_X_CLUSTER_CLIENT_IP", "HTTP_CLIENT_IP",
 			"HTTP_FORWARDED_FOR", "HTTP_FORWARDED", "HTTP_VIA", "REMOTE_ADDR" };
-	
+
 	boolean emailStatus = false;
 	boolean passStatus = false;
 
 	private DataSource dataSource;
 	private Connection con;
-	
+
 	public void init() throws ServletException {
 		try {
-			Context initContext  = new InitialContext();
-			Context envContext  = (Context)initContext.lookup("java:/comp/env");
-			dataSource = (DataSource)envContext.lookup("jdbc/usercredentialsDB");
+			Context initContext = new InitialContext();
+			Context envContext = (Context) initContext.lookup("java:/comp/env");
+			dataSource = (DataSource) envContext.lookup("jdbc/usercredentialsDB");
 		} catch (NamingException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	// Validate user email present in database or not
 	public boolean EmailValidate(String useremail) throws ServletException {
 		init();
 		try {
 			con = dataSource.getConnection();
-			String emailCheckQuery ="SELECT * from useremail WHERE user_email = ?";
-			
+			String emailCheckQuery = "SELECT * from useremail WHERE user_email = ?";
+
 			PreparedStatement ps = con.prepareStatement(emailCheckQuery);
 			ps.setString(1, useremail);
 			ResultSet rs = ps.executeQuery();
 			emailStatus = rs.next();
-			
+
 			ps.close();
 			con.close();
 		} catch (Exception e) {
-			System.out.println("EmailValidate"+e.getMessage());
+			System.out.println("EmailValidate" + e.getMessage());
 		}
 		return emailStatus;
 	}
-	
+
 	// validate password entered by user is correct or not
 	public boolean PasswordValidate(String useremail, String password) throws ServletException {
 		init();
@@ -69,42 +69,42 @@ public class UserDetailClass {
 			PreparedStatement ps = con.prepareStatement(getUserIDQuery);
 			ps.setString(1, useremail);
 			ResultSet rs = ps.executeQuery();
-			
+
 			String user_id = null;
-			if(rs.next()) {
+			if (rs.next()) {
 				user_id = rs.getString(1);
 			}
-			
-			String getByteQuery ="SELECT bytesalt from userdetail WHERE user_id = ?";
+
+			String getByteQuery = "SELECT bytesalt from userdetail WHERE user_id = ?";
 			String hashpass = null;
-			
+
 			ps = con.prepareStatement(getByteQuery);
 			ps.setString(1, user_id);
 			rs = ps.executeQuery();
-			
-			if(rs.next()) {
+
+			if (rs.next()) {
 				byte[] salt = rs.getBytes(1);
 				HashGenerator hg = new HashGenerator();
 				hashpass = hg.generateHash(password, salt);
 			}
-			
-			String validate ="SELECT * from userdetail WHERE user_id = ? and password = ?";
-			
+
+			String validate = "SELECT * from userdetail WHERE user_id = ? and password = ?";
+
 			ps = con.prepareStatement(validate);
 			ps.setString(1, user_id);
 			ps.setString(2, hashpass);
 
 			rs = ps.executeQuery();
 			passStatus = rs.next();
-			
+
 			ps.close();
 			con.close();
 		} catch (Exception e) {
-			System.out.println("PasswordValidate"+e.getMessage());
+			System.out.println("PasswordValidate" + e.getMessage());
 		}
 		return passStatus;
 	}
-	
+
 	// ADD new session into database
 	public boolean AddSession(String sessionId, String email) throws ServletException {
 		boolean sessionStatus = true;
@@ -113,15 +113,15 @@ public class UserDetailClass {
 
 			con = dataSource.getConnection();
 			String id = null;
-			
+
 			String getUserIDQuery = "SELECT user_id from useremail WHERE user_email = ?";
 			PreparedStatement ps = con.prepareStatement(getUserIDQuery);
 			ps.setString(1, email);
 			ResultSet rs = ps.executeQuery();
-			if(rs.next()) {
+			if (rs.next()) {
 				id = rs.getString(1);
 			}
-			
+
 			String setSession = "insert into usersession (user_id,session) values(?,?)";
 			ps = con.prepareStatement(setSession);
 			ps.setString(1, id);
@@ -131,23 +131,23 @@ public class UserDetailClass {
 			con.close();
 		} catch (Exception e) {
 			sessionStatus = false;
-			System.out.println("In Add Session Method "+ e.getMessage());
+			System.out.println("In Add Session Method " + e.getMessage());
 		}
 		return sessionStatus;
 	}
-	
+
 	// Fetch user id from database using session id from cookie
 	public String GetUserId(Cookie[] cookies) throws ServletException {
 		init();
 		String sessionValue = null;
-		if(cookies != null) {
-			for(Cookie cookie : cookies) {
-				if((cookie.getName()).compareTo("_Session_ID") == 0) {
+		if (cookies != null) {
+			for (Cookie cookie : cookies) {
+				if ((cookie.getName()).compareTo("_Session_ID") == 0) {
 					sessionValue = cookie.getValue();
 				}
 			}
 		}
-		
+
 		String getid = "SELECT user_id from usersession WHERE session = ?";
 		String user_id = null;
 		try {
@@ -155,20 +155,115 @@ public class UserDetailClass {
 			PreparedStatement ps = con.prepareStatement(getid);
 			ps.setString(1, sessionValue);
 			ResultSet rs = ps.executeQuery();
-			if(rs.next()) {
+			if (rs.next()) {
 				user_id = rs.getString(1);
 			}
 			ps.close();
 			con.close();
-		} 
-		catch (SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return user_id;
 	}
-	
+
+	// Fetch user id using user email
+	public String GetUserId(String useremail) throws ServletException {
+		init();
+		String user_id = null;
+		try {
+			con = dataSource.getConnection();
+			String getUserIDQuery = "SELECT user_id from useremail WHERE user_email = ?";
+
+			PreparedStatement ps = con.prepareStatement(getUserIDQuery);
+			ps.setString(1, useremail);
+			ResultSet rs = ps.executeQuery();
+
+			if (rs.next()) {
+				user_id = rs.getString(1);
+			}
+			ps.close();
+			con.close();
+		} catch (Exception e) {
+			System.out.println("sendEmailOTP " + e.getMessage());
+		}
+		return user_id;
+	}
+
+	public String GetUserName(String user_id) throws ServletException {
+		init();
+		String username = null;
+		try {
+			con = dataSource.getConnection();
+			String getUsername = "SELECT first_name, last_name from userdetail where user_id = ?";
+			PreparedStatement ps = con.prepareStatement(getUsername);
+			ps.setString(1, user_id);
+			ResultSet rs = ps.executeQuery();
+
+			if (rs.next()) {
+				username = rs.getString(1);
+				username += " ";
+				username += rs.getString(2);
+			}
+			ps.close();
+			con.close();
+		} catch (Exception e) {
+			System.out.println("sendEmailOTP " + e.getMessage());
+		}
+		return username;
+	}
+
+	public boolean setUserOTP(String user_id, String otp, String session_info) throws ServletException {
+		init();
+		try {
+			con = dataSource.getConnection();
+			String setUserOTPQuery = "insert into usermfa(user_id, otp, otp_session_info) value(?,?,?)";
+			PreparedStatement ps = con.prepareStatement(setUserOTPQuery);
+
+			ps.setString(1, user_id);
+			ps.setString(2, otp);
+			ps.setString(3, session_info);
+			ps.executeUpdate();
+			ps.close();
+			con.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+
+	public boolean userOTPValidation(String useremail, String otp, String sessionInfo) throws ServletException {
+
+		init();
+		boolean otpStatus = true;
+		String user_id = GetUserId(useremail);
+		
+		try {
+			con = dataSource.getConnection();
+			String otpValidationQuery = "SELECT * from usermfa WHERE user_id = ? and otp = ? and otp_session_info = ?";
+		
+			PreparedStatement ps = con.prepareStatement(otpValidationQuery);
+			ps.setString(1, user_id);
+			ps.setString(2, otp);
+			ps.setString(3, sessionInfo);
+			
+			ResultSet rs = ps.executeQuery();
+			rs = ps.executeQuery();
+			
+			otpStatus = rs.next();
+			ps.close();
+			con.close();
+		
+		} catch (Exception e) {
+			e.printStackTrace();
+			otpStatus = false;
+		}
+
+		return otpStatus;
+	}
+
 	// Finding User IP address from request
-	
+
 	public String UserIP(HttpServletRequest request) {
 		for (String header : HEADERS_TO_TRY) {
 			String ip = request.getHeader(header);
@@ -176,9 +271,9 @@ public class UserDetailClass {
 				return ip;
 			}
 		}
-	  return request.getRemoteAddr(); 
+		return request.getRemoteAddr();
 	}
-	
+
 }
 
 //String getbyte = "select bytesalt from usercredentials.userdetail where ? IN(email, secemail1, secemail2, secemail3);";
